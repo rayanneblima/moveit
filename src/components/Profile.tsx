1ce0d52186101450caf1a15e5482e78e04ab80a4
Rayanne <rayanne_lima2010@hotmail.com>
2021-03-02 17:28:35 -0300

    Created the Context for Countdown

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,11 @@
+import { useContext } from 'react';
+import { ChallengeContext } from '../contexts/ChallengeContext';
+
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(ChallengeContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/rayanneblima.png" alt="Rayanne B. Lima" />
@@ -8,7 +13,7 @@ export function Profile() {
         <strong>Rayanne B. Lima</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
